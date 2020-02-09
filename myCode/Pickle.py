7440a6d2e149f447de3e69e073b4752555959f00
Nick Mulhall <nick.mulhall@nam-t.com>
2020-02-09 18:32:43 +0000

    Commit Pickle Test

--- myCode/Pickle.py
+++ myCode/Pickle.py
@@ -1,4 +1,24 @@
 import pickle
-import pandas
+import pandas as pd
+import json
 
+def pickle_load(dir):
+    #df = pickle.load(open(dir + '/pickle_out.pickle'))
+    with open(dir + 'pickle_out.pickle') as f:
+       df = pickle_load(f)
+    return df
+def pickle_save(dir,frame):
+    # Two options to write  
+    with open(dir + 'pickle_out.pickle', 'wb') as f:
+        pickle.dump(frame,f)
+    #frame.to_pickle(dir + '/pickle_out.pickle')
+
+path = 'datasets/pickle/example.txt'
+records = [json.loads(line) for line in open(path)]
+frame = pd.DataFrame(records)
+directory = 'datasets/pickle/'
+#pickle_save(directory,frame)
+
+df = pickle_load(directory)
+print(df[10]) 
 
